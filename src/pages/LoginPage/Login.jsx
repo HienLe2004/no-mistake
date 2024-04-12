69567e3e1b4d4c99985b53feb3a1ddfd4e1919f5
HienLe2004 <hien.lengoc2004@hcmut.edu.vn>
2024-04-13 04:46:58 +0700

    Update login and firebase.config

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,16 +1,44 @@
 import Footer from "../../components/Footer/Footer"
+//import './auth'
 import './Login.css'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../../firebase.config'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 export default function Login() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const email = emailRef.current.value;
+            const password = passwordRef.current.value;
+            await signInWithEmailAndPassword(auth, email, password);
+            console.log("login successed");
+            emailRef.current.value = '';
+            passwordRef.current.value = '';
+            navigate("/");
+        }
+        catch(err) {
+            console.log("login failed");
+            emailRef.current.value = '';
+            passwordRef.current.value = '';
+            //console.error(err);
+        }
+    }
     return <>
         <div className="login-page">
             <section>
-                <form className="add-form-login">
+                <form onSubmit={handleSubmit} className="add-form-login">
                     <h1>Đăng nhập tài khoản DEF LMS</h1>
-                    <label>Tên tài khoản</label>
-                    <input type="text" name="email"/>
-                    <label>Mật khẩu</label>
-                    <input type="password" name="password"/>
-                    <button className="login-button"><a href="/">Đăng nhập</a></button>
+                    <label>Tên tài khoản
+                        <input type="text" name="email" ref={emailRef} required/>
+                    </label>
+                    <label>Mật khẩu
+                        <input type="password" name="password" ref={passwordRef} required/>
+                    </label>
+                    <button type="submit" className="login-button">Đăng nhập</button>
                     <a href="#" target="_blank" className="forgot-password">Quên mật khẩu?</a>
                 </form>
             </section>
