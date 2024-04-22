ed499816ab4fdc4d78c677293bb6957cd3ba9bc4
DarkStarDustVn <manh.au150304@hcmut.edu.vn>
2024-04-22 14:13:29 +0700

    Add Points v.5.0

--- src/pages/ScoringPage/ScoringPage.jsx
+++ src/pages/ScoringPage/ScoringPage.jsx
@@ -2,37 +2,91 @@ import { useState, useEffect } from "react";
 import {Helmet} from 'react-helmet-async'
 import ListofCourses from "../AddP/ListofCourses/LSC"
 import {db} from '../../../firebase.config'
-import { getDoc, doc, } from 'firebase/firestore'
+import { getDoc, doc, collection, getDocs, query,where, onSnapshot} from 'firebase/firestore'
 import { auth } from '../../../firebase.config'
+import { readTeacherData, addElementToArray, readUserDoc1, deleteElementInArray, updateField, deleteOneField } from '../../useAll/UseAll'
+import { Card, Button } from 'antd'
 
 let Newid = 0;
+function GetData({ path, rol }) {
+    const [data, setData] = useState('')
+    readTeacherData(path, rol)
+        .then((result) => {
+            setData(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    return (
+        <>
+            {data}
+        </>
+    )
+}
 export default function MyCourses() {
-    const [courses, setCourse] = useState([]);
-    const [student, setStudent] = useState([]);
-    useEffect(() => {
-        const unsub = auth.onAuthStateChanged((authObj) => {
-            unsub();
-            if (authObj) {
-                const fetchdata = async() => {
-                    const docRef = doc(db, "users", authObj.uid);
-                    const docSnap = await getDoc(docRef);
-                    let data = docSnap.data();
-                    data.courses.forEach(ref => {
-                        const fetchcourse = async() => {
-                            const files = await getDoc(ref);
-                            let course = files.data();
-                            setCourse([...courses, { id: Newid++, name: course.name}]);
-                        }
-                        fetchcourse();
-                    });
-                }
-                fetchdata();
-            } else {console.log("User not logged in")}
-        });
-    }, []);
+        //? student input | info | ref ?
+        const [student, setStudent] = useState([]);
+
+        //infor about inside
+        const [course, setCourses] = useState([])
+        const SelectedCourses = query(collection(db,'courses'),where('teacher','==',doc(db, `users/${auth.currentUser.uid}`)))
+        useEffect(() =>
+            onSnapshot(SelectedCourses, (snapshot) => {
+                setCourses(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+            }), []
+        )
+    
+        //get information about the current user for check course
+        const [information, setInformation] = useState(null)
+        useEffect(() => {
+            const unsubscribe = onSnapshot(doc(db, `users/${auth.currentUser.uid}`), (snapshot) => {
+                setInformation(snapshot.data());
+            });
+            return () => unsubscribe();
+        }, [])
+    
+        //get role of current user, 3 cases: student, teacher admin
+        const [role, setRole] = useState(null)
+        useEffect(() => {
+            readUserDoc1(auth.currentUser.uid)
+                .then(result => {
+                    setRole(result);
+                })
+                .catch(error => {
+                    console.log('not success read role')
+                });
+        }, [])
+
+
 
     return (<>
-        
-        {courses ? <ListofCourses courses={courses}/> :null}
+       <>
+            {course.map((courseEle, index) => (
+                <>
+                    <Card
+                        title={courseEle.name}
+                        bordered={false}
+                        style={{
+                            width: '50%',
+                            marginTop: '20px',
+                            textAlign: 'center',
+                            left: '25%',
+                            right: '25%',
+                            backgroundColor: '#CCE6FF',
+                            fontSize: '1rem'
+                        }}
+                        hoverable
+                        key={index}>
+                        <p>Semester: {courseEle.semester}</p>
+                        <p>Status: {courseEle.status}</p>
+                        <p>Số lượng sinh viên đã đăng ký: {'students' in courseEle ? <>{courseEle.students.length}</> : <>0</>}</p>
+                        <p>{'teacher' in courseEle ? <>Giáo viên: <GetData path={`/users/${courseEle.teacher.id}`} rol='name' /></> : <>Chưa có giáo viên đăng ký dạy</>}</p>
+                        <p>Tên của data: <GetData path={`course/${courseEle.id}/data/key`} rol='name' /></p>
+                        <p>Mô tả: <GetData path={`course/${courseEle.id}/data/key`} rol='description' /></p>
+                    </Card>
+                </>
+            ))}
+        </>
+       
     </>)
 }
