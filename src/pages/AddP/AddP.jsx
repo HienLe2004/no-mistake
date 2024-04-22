7888c1a4c419672db9ed4f2ee6a98218d554399f
DarkStarDustVn <manh.au150304@hcmut.edu.vn>
2024-04-22 16:45:05 +0700

    Not Imp

--- src/pages/AddP/AddP.jsx
+++ src/pages/AddP/AddP.jsx
@@ -8,90 +8,141 @@ import { readTeacherData, addElementToArray, readUserDoc1, deleteElementInArray,
 import { NavLink,Link, useNavigate } from "react-router-dom";
 import { Card, Button } from 'antd'
 
-let nextid = 0;
-export default function MyCourses() {
-    
-    function GetData({ path, rol }) {
-        const [data, setData] = useState('')
-        readTeacherData(path, rol)
-            .then((result) => {
-                setData(result)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-        return (
-            <>
-                {data}
-            </>
-        )
-    }
- 
-            //? student input | info | ref ?
-            const [student, setStudent] = useState([]);
-    
-            //infor about inside
-            const [course, setCourses] = useState([])
-            const SelectedCourses = query(collection(db,'courses'),where('teacher','==',doc(db, `users/${auth.currentUser.uid}`)))
-            useEffect(() =>
-                onSnapshot(SelectedCourses, (snapshot) => {
-                    setCourses(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
-                }), []
-            )
+function GetData({ path, rol }) {
+    const [data, setData] = useState('')
+    readTeacherData(path, rol)
+        .then((result) => {
+            setData(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    return (
+        <>
+            {data}
+        </>
+    )
+}
+
+    let nextid = 0;
+    export default function MyCourses() {
+
+        //infor about inside
+        const [course, setCourses] = useState([])
+        const SelectedCourses = query(collection(db,'courses'),where('teacher','==',doc(db, `users/${auth.currentUser.uid}`)))
+        useEffect(() =>
+            onSnapshot(SelectedCourses, (snapshot) => {
+                setCourses(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+            }), [])
         
-            //get information about the current user for check course
-            const [information, setInformation] = useState(null)
-            useEffect(() => {
-                const unsubscribe = onSnapshot(doc(db, `users/${auth.currentUser.uid}`), (snapshot) => {
-                    setInformation(snapshot.data());
+
+         //? student input | info | ref ?
+        const [student, setStudent] = useState([]);
+       
+        useEffect(() =>
+            onSnapshot(SelectedCourses, (snapshot) => {
+                setStudent(snapshot.docs.map(doc => ({ ...doc.data().students, id: doc.id })));
+            }), [])
+        console.log(student);
+
+        //get information about the current user for check course
+        const [information, setInformation] = useState(null)
+        useEffect(() => {
+            const unsubscribe = onSnapshot(doc(db, `users/${auth.currentUser.uid}`), (snapshot) => {
+                setInformation(snapshot.data());
+            });
+            return () => unsubscribe();
+        }, [])
+        
+        //get role of current user, 3 cases: student, teacher admin
+        const [role, setRole] = useState(null)
+        useEffect(() => {
+            readUserDoc1(auth.currentUser.uid)
+                .then(result => {
+                     setRole(result);
+                })
+                .catch(error => {
+                    console.log('not success read role')
                 });
-                return () => unsubscribe();
-            }, [])
-        
-            //get role of current user, 3 cases: student, teacher admin
-            const [role, setRole] = useState(null)
-            useEffect(() => {
-                readUserDoc1(auth.currentUser.uid)
-                    .then(result => {
-                        setRole(result);
-                    })
-                    .catch(error => {
-                        console.log('not success read role')
-                    });
-            }, [])
-            const navigate = useNavigate();
+        }, [])
+        const navigate = useNavigate();
     
     
         return (<>
            <>
-                {course.map((courseEle, index) => (
-                    <>
-                        <Card
-                            title={courseEle.name}
-                            bordered={false}
-                            style={{
-                                width: '50%',
-                                marginTop: '20px',
-                                textAlign: 'center',
-                                left: '25%',
-                                right: '25%',
-                                backgroundColor: '#CCE6FF',
-                                fontSize: '1rem'
-                            }}
-                            hoverable
-                            key={index}>
-                            <p>Semester: {courseEle.semester}</p>
-                            <p>Status: {courseEle.status}</p>
-                            <p>Số lượng sinh viên đã đăng ký: {'students' in courseEle ? <>{courseEle.students.length}</> : <>0</>}</p>
-                            <p>{'teacher' in courseEle ? <>Giáo viên: <GetData path={`/users/${courseEle.teacher.id}`} rol='name' /></> : <>Chưa có giáo viên đăng ký dạy</>}</p>
-                            <p>Tên của data: <GetData path={`course/${courseEle.id}/data/key`} rol='name' /></p>
-                            <p>Mô tả: <GetData path={`course/${courseEle.id}/data/key`} rol='description' /></p>
-                            <Button onClick={() => {navigate('/scoringpage')}} > Change Student Points </Button>
-                        </Card>
-                    </>
-                ))}
+                {course.map((courseEle, index) => {
+                    
+                        return (
+                            <>
+                                <Card
+                                    title={courseEle.name}
+                                    bordered={false}
+                                    style={{
+                                        width: '50%',
+                                        marginTop: '20px',
+                                        textAlign: 'center',
+                                        left: '25%',
+                                        right: '25%',
+                                        backgroundColor: '#CCE6FF',
+                                        fontSize: '1rem'
+                                    }}
+                                    hoverable
+                                    key={index}>
+                                    <p>Semester: {courseEle.semester}</p>
+                                    <p>Status: {courseEle.status}</p>
+                                    <p>Số lượng sinh viên đã đăng ký: {'students' in courseEle ? <>{courseEle.students.length}</> : <>0</>}</p>
+                                    <p>Sinh Viên: {'students' in courseEle ? <>{courseEle.students.length}</> : <>None</>}</p>
+                                    <p>{'teacher' in courseEle ? <> Giáo viên: <GetData path={`/users/${courseEle.teacher.id}`} rol='name' /></> : <>Chưa có giáo viên đăng ký dạy</>}</p>
+                                    <p>Tên của data: <GetData path={`course/${courseEle.id}/data/key`} rol='name' /></p>
+                                    <p>Mô tả: <GetData path={`course/${courseEle.id}/data/key`} rol='description' /></p>
+                                    <Button onClick={() => { navigate('/scoringpage'); } }> Change Student Points </Button>
+                                </Card>
+
+                                
+                            </>
+                            
+                        );
+                    
+                })}
             </>
-           
+
+        <>
+            {student.map((courseEle, index) => {
+                    
+                    return (
+                        <>
+                            <Card
+                                title={courseEle.name}
+                                bordered={false}
+                                style={{
+                                    width: '50%',
+                                    marginTop: '20px',
+                                    textAlign: 'center',
+                                    left: '25%',
+                                    right: '25%',
+                                    backgroundColor: '#CCE6FF',
+                                    fontSize: '1rem'
+                                }}
+                                hoverable
+                                key={index}>
+                                <p>Semester: {courseEle.semester}</p>
+                                <p>Status: {courseEle.status}</p>
+                                <p>Số lượng sinh viên đã đăng ký: {'students' in courseEle ? <>{courseEle.students.length}</> : <>0</>}</p>
+                                <p>{'students' in courseEle ? <> Sinh viên: <GetData path={`/users/${courseEle.students.id}`} rol='name' /></> : <>Chưa có giáo viên đăng ký dạy</>}</p>
+                                <p>{'teacher' in courseEle ? <> Giáo viên: <GetData path={`/users/${courseEle.teacher.id}`} rol='name' /></> : <>Chưa có giáo viên đăng ký dạy</>}</p>
+                                <p>Tên của data: <GetData path={`course/${courseEle.id}/data/key`} rol='name' /></p>
+                                <p>Mô tả: <GetData path={`course/${courseEle.id}/data/key`} rol='description' /></p>
+                                <Button onClick={() => { navigate('/scoringpage'); } }> Change Student Points </Button>
+                            </Card>
+
+                            
+                        </>
+                        
+                    );
+                
+            })}
+        </>
+
         </>)
     
     
