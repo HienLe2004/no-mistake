67e32d74b2cb1c7ba27cd176c2644a60c6dce0e8
Nguyen Loc <loc312004@gmail.com>
2024-04-22 21:35:35 +0700

    Small changes in CourseList

--- src/pages/MyCourses/CourseList/CourseList.jsx
+++ src/pages/MyCourses/CourseList/CourseList.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import './CourseList.css'
-
+import { useNavigate } from "react-router-dom";
 export default function CourseList({courses}) {
+    let navigate = useNavigate(); 
+    const handleClick = () =>{ 
+        let path = `newPath`; 
+        navigate(path);
+    }
     return <>
         <div className="title">Danh sách khóa học</div>
         <div className="card-around">
             {courses.map(course => (
-                <button /*onClick={}*/ className="card-container" key={course.nextid}>
+                <button /*onClick={handleClick}*/ className="card-container" key={course.nextid}>
                     <img src="https://picsum.photos/250/100" className="card-img"/>
                     <h1 className="card-title">{course.name}</h1>
                     {/* <p className="card-description">Giảng viên: {course.teacher}</p> */}
